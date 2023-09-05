5d3d16e71aa20b5b986ba88a42a1f1ca868d8549
Roman Oleksiuk <checkyourmath@gmail.com>
2023-09-05 14:37:56 +0200

    Button test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">
-      Hello
-    </p>
+    <div>
+      <p className="text-3xl font-bold text-indigo-500">
+        Hello
+      </p>
+      <Button
+        variant="ghost"
+      >
+        Click Me
+      </Button>
+    </div>
   )
 }
