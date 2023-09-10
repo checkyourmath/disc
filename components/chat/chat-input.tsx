545eba05fdebc6810a1ad8b5deaaa485b48e882f
Roman Oleksiuk <checkyourmath@gmail.com>
2023-09-10 18:29:12 +0200

    Fixed: chat input disabled is not working

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -39,6 +39,10 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (isLoading || !values.content.trim()) {
+      return;
+    }
+
     try {
       const url = qs.stringifyUrl({
         url: apiUrl,
@@ -68,13 +72,14 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
+                    disabled={isLoading}
                     onClick={() => openDialog(DialogType.MESSAGE_FILE, { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
                   </button>
                   <Input
-                    disabled={isLoading}
+                    isDisabled={isLoading}
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                     placeholder={`Message ${type === "conversation" ? name : "#" + name}`}
                     {...field}
