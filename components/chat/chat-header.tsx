a9fcb4ca1ea263ecb748bca29ba1bb7af745fe25
Roman Oleksiuk <checkyourmath@gmail.com>
2023-09-11 12:30:25 +0200

    Correction

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -22,7 +22,7 @@ const iconMap = {
 
 export const ChatHeader = ({ serverId, name, channelType, type, imageUrl }: ChatHeaderProps) => {
   return (
-    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+    <div className="text-md font-semibold flex px-3 items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobileToggle serverId={serverId} />
       {type === "channel" && channelType && iconMap[channelType]}
       {type === "conversation" && (
